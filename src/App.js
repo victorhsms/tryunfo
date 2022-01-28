9b9d1c6e4f20a57a7e2598830c2dac8fba9c9e4f
Victor Hugo <victorhsmsantos@gmail.com>
2022-01-28 17:44:20 -0300

    Requisito 3 feito!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   render() {
     return (
-      <Form
-        cardName="App"
-        cardDescription=""
-        cardAttr1=""
-        cardAttr2=""
-        cardAttr3=""
-        cardImage=""
-        cardRare=""
-        cardTrunfo=""
-        hasTrunfo={ false }
-        isSaveButtonDisabled={ false }
-        onInputChange={ () => {} }
-        onSaveButtonClick={ () => {} }
-      />
+      <div>
+        <Form
+          cardName=""
+          cardDescription=""
+          cardAttr1=""
+          cardAttr2=""
+          cardAttr3=""
+          cardImage=""
+          cardRare=""
+          cardTrunfo={ false }
+          hasTrunfo={ false }
+          isSaveButtonDisabled={ false }
+          onInputChange={ () => {} }
+          onSaveButtonClick={ () => {} }
+        />
+        <Card
+          cardName=""
+          cardDescription=""
+          cardAttr1=""
+          cardAttr2=""
+          cardAttr3=""
+          cardImage=""
+          cardRare=""
+          cardTrunfo=""
+        />
+      </div>
     );
   }
 }
